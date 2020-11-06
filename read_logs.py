acd7a1a1b6d9147907f7dd3e6be0da99a0f22e62
Zeb Engberg <zebengberg@gmail.com>
2020-11-05 20:10:09 -0700

    small bug fix

--- read_logs.py
+++ read_logs.py
@@ -3,6 +3,7 @@
 import tensorflow as tf
 import glob
 import pandas as pd
+import numpy as np
 
 
 def build_df():
@@ -11,6 +12,7 @@ def build_df():
   speeds = []
   episodes = []
   scores = []
+  step_sizes = []
   for path in log_data:
     # not yet implemented in v2?
     data_from_path = tf.compat.v1.train.summary_iterator(path)
@@ -21,23 +23,36 @@ def build_df():
           step = e.step
           speeds.append(speed)
           episodes.append(step)
-        if v.tag == 'learning_score':
+        elif v.tag == 'learning_score':
           score = tf.make_ndarray(v.tensor).item()
           scores.append(score)
+        elif v.tag == 'step_size':
+          size = tf.make_ndarray(v.tensor).item()
+          step_sizes.append(size)
 
-  df = pd.DataFrame({'episode': episodes, 'speed': speeds, 'score': scores})
+  df = pd.DataFrame({'episode': episodes, 'speed': speeds,
+                     'score': scores, 'step_size': step_sizes})
   df.set_index('episode', inplace=True)
   df.sort_index(inplace=True)
   return df
 
 
 def is_progress_made():
-  """Determine if any progress has been made over last 10_000 episodes."""
-  num_episodes = 10_000
+  """Determine if any progress has been made over recent episodes."""
   df = build_df()
+
+  initial_step_size = df['step_size'].loc[0]
   current_episode = df.index[-1]
-  if current_episode <= num_episodes:
+  current_step_size = df['step_size'].loc[current_episode]
+
+  # the log approximates how many step_size reductions have been made
+  num_episodes = int(1e4 * np.log(1 + initial_step_size / current_step_size))
+  print(num_episodes)
+
+  # only looking at entries with step_size equal to current value
+  df = df[df['step_size'] == current_step_size]
+  initial_episode = df.index[0]
+  if current_episode - initial_episode < num_episodes:
     return True
-  previous_episode = current_episode - num_episodes
-  scores = df['score'].loc[previous_episode:current_episode]
-  return scores.sum() > 0
+  df = df.loc[current_episode - num_episodes: current_episode]
+  return df['score'].sum() > 0
